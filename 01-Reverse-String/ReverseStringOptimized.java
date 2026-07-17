105ed93b2ead3ed9cd2ce91cab30c03d33416c69
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-17 19:24:07 +0530

    Add optimized string reversal implementation

---
+++ 01-Reverse-String/ReverseStringOptimized.java
@@ -0,0 +1,17 @@
+public class ReverseStringOptimized {
+    public static void main(String[] args) {
+        String str = "Rahul";
+        char[] arr = str.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < right) {
+            char temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.println(new String(arr));
+    }
+}
+//Two pointers
