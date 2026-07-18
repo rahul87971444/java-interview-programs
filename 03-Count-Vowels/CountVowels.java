977132fa46a1151d4c75a1b0ba2218bf30a8e417
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-19 04:37:29 +0530

    Refactor vowel counting in CountVowels.java
    Refactor vowel counting logic to improve clarity and efficiency.

---
+++ 03-Count-Vowels/CountVowels.java
@@ -0,0 +1,15 @@
+public class CountVowels {
+    public static void main(String[] args) {
+        String str = "Education";
+        int count = 0;
+        str = str.toLowerCase();
+        for(int i = 0; i < str.length(); i++){
+            char ch = str.charAt(i);
+            if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+                count++;
+            }
+        }
+        System.out.println("Number of vowels = " + count);
+
+    }
+}
