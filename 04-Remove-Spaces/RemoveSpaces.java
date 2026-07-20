4b718409c3d624ba40a9a08dfb84d620f0964650
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-20 09:43:22 +0530

    Add RemoveSpaces class to remove spaces from a string

---
+++ 04-Remove-Spaces/RemoveSpaces.java
@@ -0,0 +1,7 @@
+public class RemoveSpaces {
+    public static void main(String[] args) {
+        String str = "Java Interview Preparation";
+        str = str.replace(" ", "");
+        System.out.println(str);
+    }
+}
