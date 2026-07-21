a677eb9c8df3ef5d891b2a5c1083b49c0f78fdf0
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-21 09:45:00 +0530

    Add CharacterFrequency class to count character frequency

---
+++ 05-Character-Frequency/CharacterFrequency.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+public class CharacterFrequency {
+    public static void main(String[] args) {
+        String str = "banana";
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char ch : str.toCharArray()){
+            map.put(ch, map.getOrDefault(ch,0)+1);
+        }
+        for(char key : map.keySet()){
+            System.out.println(key + " = " + map.get(key));
+        }
+    }
+}
