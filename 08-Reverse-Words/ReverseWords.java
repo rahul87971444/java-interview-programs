1c58a0b67b09ad0b373c282bfae3cf981d76fd9e
Rahul Kumar <rahulkumar87971444@gmail.com>
2026-07-26 21:06:39 +0530

    Add ReverseWords class to reverse words in a string

---
+++ 08-Reverse-Words/ReverseWords.java
@@ -0,0 +1,15 @@
+public class ReverseWords {
+    public static void main(String[] args) {
+        String str = "Java is Awesome";
+        String words[] = str.split(" ");
+        for(String word : words){
+            for(int i = word.length()-1; i>=0; i--){
+                System.out.print(word.charAt(i));
+            }
+            System.out.print(" ");
+
+        }
+
+    }
+
+}
